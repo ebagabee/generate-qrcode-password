289174b157f8e476daab31381d1cf15f192be7c1
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-08-07 14:35:23 -0300

    Criando QRCode

--- src/prompts/prompt-qrcode.js
+++ src/prompts/prompt-qrcode.js
@@ -1,7 +1,16 @@
 import chalk from "chalk";
 
 const promptQRCode = [
-    
+    {
+        name: "link",
+        description: chalk.yellow("Digite o link para gerar o QRCode"),
+    },
+    {
+        name: "type",
+        description: chalk.yellow("Escolha entre o tipo (1 - NORMAL OU 2 - TERMINAL)"),
+        pattern: /^[1-2]+$/,
+        message: chalk.red.italic("Escolha apenas entre 1 e 2")
+    }
 ];
 
 export default promptQRCode;
