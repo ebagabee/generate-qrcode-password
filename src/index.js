2f8bc4ef07513d27d5f2c4b35cf487deee68564b
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-08-06 23:38:37 -0300

    Finalizando primeiro prompt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import prompt from "prompt";
 
+import mainPrompt from "./prompts/prompt-main.js";
+
 async function main() {
-    
+    prompt.get(mainPrompt, async (err, choose) => {
+        if (choose.select == 1) console.log("escolheu o QRCODE");
+        if (choose.select == 2) console.log("escolheu o Password");
+    });
+
+    prompt.start();
 }
 
 main();
